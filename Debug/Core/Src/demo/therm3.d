7e509a5faf63afe000a5f6bbe46cebcc75d83339
Kolia Lolik <124440379+Marra3888@users.noreply.github.com>
2026-03-09 15:48:40 +0200

    Add files via upload

---
+++ Debug/Core/Src/demo/therm3.d
@@ -0,0 +1,7 @@
+Core/Src/demo/therm3.o: ../Core/Src/demo/therm3.c \
+ ../Core/Src/demo/therm3.h ../Core/Src/display/dispcolor.h \
+ ../Core/Src/display/fonts/font.h ../Core/Src/gc9a01a/gc9a01a.h
+../Core/Src/demo/therm3.h:
+../Core/Src/display/dispcolor.h:
+../Core/Src/display/fonts/font.h:
+../Core/Src/gc9a01a/gc9a01a.h:
