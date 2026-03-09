e2cf04034a3b1399b40fe27bf11fecb5082dc8f5
Kolia Lolik <124440379+Marra3888@users.noreply.github.com>
2026-03-09 15:43:02 +0200

    Add files via upload

---
+++ Debug/Core/Src/AnimatedGIF.d
@@ -0,0 +1,3 @@
+Core/Src/AnimatedGIF.o: ../Core/Src/AnimatedGIF.c \
+ ../Core/Src/AnimatedGIF.h
+../Core/Src/AnimatedGIF.h:
---
+++ Debug/Core/Src/main.d
@@ -0,0 +1,106 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Core/Inc/stm32f4xx_hal_conf.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ ../Core/Src/display/dispcolor.h ../Core/Src/gpio/gpio.h \
+ ../Core/Src/demo/hvac.h ../Core/Src/demo/htp.h ../Core/Src/demo/therm3.h \
+ ../Core/Src/demo/power.h ../Core/Src/demo/horse_frames.h \
+ ../Core/Src/demo/voltage_meter.h ../Core/Src/demo/tachometer.h \
+ ../Core/Src/demo/rainbow.h ../Core/Src/demo/mono.h \
+ ../Core/Src/demo/humidity_meter.h ../Core/Src/demo/bodmer_spiral.h \
+ ../Core/Src/demo/ampelmann.h \
+ ../Core/Src/demo/three_orbiting_rotating_yinyang.h \
+ ../Core/Src/demo/bodmer_single_yinyang.h ../Core/Src/demo/sht21_mode.h \
+ ../Core/Src/demo/benchmark_mode.h ../Core/Src/demo/watchface.h \
+ ../Core/Src/demo/rtc_print_mode.h ../Core/Src/demo/smooth_clock.h \
+ ../Core/Src/demo/boat_gauges.h ../Core/Src/demo/clock.h \
+ ../Core/Src/demo/compas.h ../Core/Src/demo/textfading.h \
+ ../Core/Src/demo/thermostat.h ../Core/Src/display/fonts/font.h \
+ ../Core/Inc/w25qxx.h ../Core/Src/demo/gif_mode.h
+../Core/Inc/main.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Core/Inc/stm32f4xx_hal_conf.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+../Core/Src/display/dispcolor.h:
+../Core/Src/gpio/gpio.h:
+../Core/Src/demo/hvac.h:
+../Core/Src/demo/htp.h:
+../Core/Src/demo/therm3.h:
+../Core/Src/demo/power.h:
+../Core/Src/demo/horse_frames.h:
+../Core/Src/demo/voltage_meter.h:
+../Core/Src/demo/tachometer.h:
+../Core/Src/demo/rainbow.h:
+../Core/Src/demo/mono.h:
+../Core/Src/demo/humidity_meter.h:
+../Core/Src/demo/bodmer_spiral.h:
+../Core/Src/demo/ampelmann.h:
+../Core/Src/demo/three_orbiting_rotating_yinyang.h:
+../Core/Src/demo/bodmer_single_yinyang.h:
+../Core/Src/demo/sht21_mode.h:
+../Core/Src/demo/benchmark_mode.h:
+../Core/Src/demo/watchface.h:
+../Core/Src/demo/rtc_print_mode.h:
+../Core/Src/demo/smooth_clock.h:
+../Core/Src/demo/boat_gauges.h:
+../Core/Src/demo/clock.h:
+../Core/Src/demo/compas.h:
+../Core/Src/demo/textfading.h:
+../Core/Src/demo/thermostat.h:
+../Core/Src/display/fonts/font.h:
+../Core/Inc/w25qxx.h:
+../Core/Src/demo/gif_mode.h:
